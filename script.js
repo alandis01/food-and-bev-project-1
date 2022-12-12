1e131326d4b380fd405f354240f97f9bc3105f04
kassrojas <kassandrarojass@gmail.com>
2022-12-12 14:08:03 -0500

    added ids for radio buttons, started function in javascript

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 // http://www.thecocktaildb.com/api/json/v1/1/search.php?i=vodka
 // http://www.themealdb.com/api/json/v1/1/filter.php?i=chicken_breast
+var searchButtonEl = document.querySelector('#searchButton');
+var userSelectEl = document.querySelector('#userSelect');
+
+
+
+fetch(requestUrl)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
